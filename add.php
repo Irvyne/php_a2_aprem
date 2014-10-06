e264ce8527189f67ff195dc13214d8a0d77d4e5e
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-10-06 17:38:04 +0200

    Add

---
+++ add.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Thibaud BARDIN (https://github.com/Irvyne).
+ * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
+ */
+
+function add($a, $b, $c = 0) {
+    return $a+$b+$c;
+}
+
+echo add(4, 8); // 12
