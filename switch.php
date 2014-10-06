8fe44fc468d72d5f8a30bfc8284632b29000e9eb
Thibaud BARDIN <thibaud.bardin@gmail.com>
2014-10-06 17:24:39 +0200

    Switch

---
+++ switch.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Thibaud BARDIN (https://github.com/Irvyne).
+ * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
+ */
+
+$i = mt_rand(1, 5);
+
+switch ($i) {
+    case 1:
+        echo 'C 1';
+        break;
+    case 2:
+        echo 'C 2';
+        break;
+    case 3:
+        echo 'C 3';
+        break;
+    default:
+        echo 'C ri1';
+}
